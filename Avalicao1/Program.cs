62bfdd9b43f236f32b16a8a98796ccb86e8cf4a5
Dev4Jobs <devjobs@gmail.com>
2019-06-15 16:28:56 -0300

    Start

---
+++ Avalicao1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Avalicao1
+{
+    class Program
+    {
+        static string[] alunos = new string[5];
+        static int[] notas = new int[5];
+
+        // Bloco inicial do programa - Captura das informações com validações dos valores das notas informados
+        static void Main(string[] args)
+        {
+            int notasInformadas = 0;
+
+            while (notasInformadas < 5)
+
+            {
+                Console.WriteLine("Informe o nome do aluno");
+                alunos[notasInformadas] = Console.ReadLine();
+
+
+                try
+                {
+                    Console.WriteLine("Informe a nota do aluno");
+                    int nota = Convert.ToInt32(Console.ReadLine());
+
+                    while (!(nota >= 0 && nota <= 10))
+                    {
+                        Console.WriteLine("Valor inválido para nota. Informa uma nota válida!");
+                        nota = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    notas[notasInformadas] = nota;
+                    notasInformadas++;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Valor inválido para nota!");
+                    //
+                    alunos[notasInformadas].Remove(0);
+
+                }
+
+            }
+
+            Turma.CalculaMediaDaTurma(notas);
+            Turma.CalculaAprovadosEReprovados(notas);
+            Console.WriteLine();
+            Turma.ImprimeResultado(alunos, notas);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Avalicao1/Turma.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Avalicao1
+{
+    public static class Turma
+    {
+        // Calcula a média da turma
+        public static void CalculaMediaDaTurma(int[] notasAlunos)
+        {
+            int somaNotas = 0;
+            foreach (var nota in notasAlunos)
+                somaNotas += nota;
+
+            Console.WriteLine($"A média da turma é de:{somaNotas / notasAlunos.Length}");
+        }
+
+        // Calcula % de aprovados e reprovados
+        public static void CalculaAprovadosEReprovados(int[] notasAlunos)
+        {
+            int aprovados = 0;
+            int reprovados = 0;
+
+            foreach (var nota in notasAlunos)
+                if (nota >= 5)
+                    aprovados++;
+                else
+                    reprovados++;
+
+            Console.WriteLine($"{aprovados * 20}% dos alunos foram aprovados!");
+            Console.WriteLine($"{reprovados * 20}% dos alunos foram reprovados!");
+        }
+
+
+        // Aluno aprovado ou reprovado?
+        public static string AprovadoOuReprovado(int nota)
+        {
+            if (nota >= 5)
+                return "Aprovado";
+            else
+                return "Reprovado";
+        }
+
+
+        // Imprime o nome e a nota do aluno e se foi aprovado ou reprovado
+        public static void ImprimeResultado(string[] alunos, int[] notasAlunos)
+        {
+            for (int i = 0; i < alunos.Length; i++)
+                Console.WriteLine($"{alunos[i]} tirou a nota {notasAlunos[i]} e foi: {AprovadoOuReprovado(notasAlunos[i])}");
+
+        }
+    }
+}
